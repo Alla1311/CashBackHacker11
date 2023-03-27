f6a89f021b369352ef2b0f8afcc80bacd8b68dfc
Alla <lialia13111989@mail.ru>
2023-03-28 01:11:41 +0300

    new commit

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,2 +1,20 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashBackHacker;
+
 public class CashbackHackServiceTest {
+
+    @Test
+
+    public void shouldRemain100IfAmount900() {
+
+        CashBackHacker service = new CashBackHacker();
+
+        int amount = 900;
+        int expected = 300;
+        int actual = service.remain(900);
+
+        Assert.assertEquals(actual, expected);
+
+    }
 }
